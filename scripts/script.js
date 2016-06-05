1b8dfd11f556626e8efcdd3d0b76a2bfd05690a8
mculotta120 <margaret.culotta@gmail.com>
2016-06-05 15:39:00 -0500

    mysteriously working... but showing 2 of everything.

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,62 @@
-var studentPos = 0;
 
+var studentPos = 0;
 $(function(){
-  $('#start').on('click', function(){
-    console.log( 'click!' );
+    console.log( 'begin' );
      $.ajax({
        url: 'https://raw.githubusercontent.com/devjanaprime/2.4-jQueryAjaxJSON/master/students.json',
        dataType: 'json',
        success: function( data ){
-         var firstName = data.students[studentPos].first_name;
-         var lastName = data.students[studentPos].last_name;
-         var city = data.students[studentPos].city;
-         var shoutout = data.students[studentPos].shoutout;
-          console.log( 'in ajax success' );
-          console.log( data );
-          var studentInfo = '<p>'+ "Name: "+ firstName + " " + lastName + " City: " + city + " Shoutout: " + shoutout + '</p>';
-          $('#infoDisplay').append(studentInfo);
-
-          //prev button
-          $('#prev').click(function(){
-            console.log("previous clicked");
-            studentPos--;
-              console.log(studentPos);
-              $('#infoDisplay').append(studentInfo);
-            });  //end prev
-
-          //next button
-          $('#next').click(function(){
-            console.log("next clicked");
-            studentPos++;
-            console.log(studentPos);
-          }); //next
-
+         console.log( 'in ajax success' );
+         console.log( data );
+         classJson = data;
+         main ();
          }, // end success
        statusCode: {
           404: function(){
              alert( 'error connecting to server' );
           } // end 404
          } // end statusCode
-       }); // end ajax  object
-    });//end start button
+      }); // end ajax  object
 });//end all
+
+function main (){
+  console.log("main is called");
+       showNumber(studentPos);
+
+
+       //next button
+       $('#next').click(function(){
+         studentPos++;
+         console.log(studentPos);
+         if(studentPos == 21){
+             studentPos = 0;
+           }
+        showStudent(studentPos);
+
+
+       }); //next
+       //prev button
+       $('#prev').click(function(){
+          studentPos--;
+          console.log(studentPos);
+          if(studentPos == -1){
+              studentPos = 20;
+            }
+          showStudent(studentPos);
+         });  //end prev
+
+
+      function showStudent (number){
+        var firstName = classJson.students[number].first_name;
+        var lastName = classJson.students[number].last_name;
+        var city = classJson.students[number].city;
+        var shoutout = classJson.students[number].shoutout;
+        var studentInfo =  "Name: "+ firstName + " " + lastName + " City: " + city + " Shoutout: " + shoutout;
+          console.log("showStudent is called");
+          $('p').text(studentInfo).fadeIn(3000);
+
+ } //end showStudent
+  function showNumber (number){
+     $('#infoDisplay').append('<p>'+"# " + number  + " of 20"+'</p>');
+  }
+} // end main
